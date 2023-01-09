c899a073850bd5b5a3b6d185c3ca1b93c0e45ab0
Camilo Galezo <71099697+CGalezo@users.noreply.github.com>
2023-01-09 16:29:20 -0500

    Add root route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('root');
+// Redux
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+
+// React Router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Routes
+import Root from "./routes/root";
+
+// CSS
+import "./index.css";
+
+const container = document.getElementById("root");
 const root = createRoot(container);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
---
+++ src/routes/root.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+// MUI Components
+import { Box } from '@mui/material'
+
+// Components
+import Navbar from '../components/Navbar'
+
+const Root = () => {
+  return (
+    <Box>
+      <Navbar/>
+    </Box>
+  )
+}
+
+export default Root
