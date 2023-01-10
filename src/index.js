6dde3296ea640351a3de16e9f698a7943bb3031b
Camilo Galezo <71099697+CGalezo@users.noreply.github.com>
2023-01-10 02:57:36 -0500

    Add Home, Images, Ducks, Register and Edit routes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import { createRoot } from "react-dom/client";
 
 // Redux
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { store } from "./store";
 
 // React Router
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Routes
 import Root from "./routes/root";
+import Error from "./components/Error";
+import Home from "./routes/home";
+import Ducks from "./routes/ducks";
+import Images from "./routes/images";
+import Register from "./routes/register";
+import Edit from "./routes/edit";
 
 // CSS
 import "./index.css";
@@ -17,10 +23,33 @@ import "./index.css";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-const router = createBrowserRouter([
+const router = createBrowserRouter([ // React router routes for navigation through web page
   {
     path: "/",
     element: <Root />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true, // Default view, it means "/" route
+        element: <Home />,
+      },
+      {
+        path: "/images",
+        element: <Images />,
+      },
+      {
+        path: "/ducks",
+        element: <Ducks />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/edit/:duckId",
+        element: <Edit />,
+      },
+    ],
   },
 ]);
 
